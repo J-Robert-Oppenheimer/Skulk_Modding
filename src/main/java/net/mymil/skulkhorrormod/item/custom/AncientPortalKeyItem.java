a0c47ad38b60441613f0dab8aa61c8e8dbb323ee
J-Robert-Oppenheimer <mymilliot@aol.com>
2024-01-03 21:10:57 -0500

    First Commit

---
+++ src/main/java/net/mymil/skulkhorrormod/item/custom/AncientPortalKeyItem.java
@@ -0,0 +1,17 @@
+package net.mymil.skulkhorrormod.item.custom;
+
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.context.UseOnContext;
+
+public class AncientPortalKeyItem extends Item   {
+    public AncientPortalKeyItem(Properties pProperties) {
+
+        super(pProperties);
+    }
+
+    @Override
+    public InteractionResult useOn(UseOnContext pContext) {
+        return super.useOn(pContext);
+    }
+}
